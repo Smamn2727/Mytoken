49a76f09e8dd884eb08d0e9aa8d1d4f2f6b02c78
Smamn2727 <104695386+Smamn2727@users.noreply.github.com>
2023-07-30 22:10:06 +0200

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf74326baf85a59ad8282093938ef8ddf0644449f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
